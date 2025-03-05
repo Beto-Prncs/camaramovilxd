4fe53035aa9eed64d446930180224c178ab82567
Beto-Prncs <sebascastrodelgado@gmail.com>
2025-03-05 12:00:02 -0500

    sexond commit message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CameraComponent } from './camera/camera.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,CameraComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appcamara';
+  title = 'camera-app';
 }
